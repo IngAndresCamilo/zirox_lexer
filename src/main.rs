45beeb8c09845c43bef1270d1c21b1015ecb9ab7
Andrés Camilo Tobon <sadmanopsod@gmail.com>
2022-07-01 06:25:05 -0500

    Fixing Read built-in command

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,40 @@ use std::fs::File;
 /*
     read_file - Read the content of a existing file
  */
-fn read_file(path: &str) -> Result<String, std::io::Error> {
+fn read_file(path: &str) -> Result<usize, std::io::Error> {
     let file = File::open(path)?;
-    let buff = BufReader::new(file);
-    let mut buffer = String::new();
+    let buff = BufReader::new(&file);
+    let mut bytes_number: usize = 0;
+
+    println!("File path: {}\n- - - - - - - - - - - - -", path);
 
     for line in buff.lines() {
-        let b = (line.unwrap() + &"".to_string());
+        let b = line.unwrap() + &"".to_string();
         println!("{}", b);
+        bytes_number += b.len();
     }
-
-    Ok(buffer)
+    Ok(bytes_number)
 }
 
-fn read_file_callback(command_args: Vec<String>) {
-    // required argv 1
-}
 
 fn read_stdin() -> Result<u64, std::io::Error> {
     let event_return = 'EventLoop: loop {
-        let command_parsed: Vec<String> = Vec::new();
 
         println!("Zirox Lexer 0.0.1");
 
         print!("Configuration Loaded successfully> ");
         stdout().flush().unwrap();
         for line in stdin().lines() {
-            match line.unwrap().as_str() {
+
+            let cmd_parsed: Vec<String> = line.unwrap().split(" ")
+                            .map(|val| val.to_string())
+                            .collect();
+
+            match cmd_parsed.first().unwrap().as_str() {
+                "Read" => {
+                    let b= read_file(cmd_parsed[1].as_str())?;
+                    println!("- - - - - - - - - - - - -\nBytes:{}", b);
+                }
                 "Exit" => break 'EventLoop,
                 _ => println!("Command or binary not found"),
             }
