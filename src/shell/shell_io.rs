bc89c8a2d395bebf2a3ecad521cc8fddb321c2b1
Deepzirox <toboncamilo33@gmail.com>
2022-07-04 07:19:51 -0500

    Improving structure and code

--- src/shell/shell_io.rs
+++ src/shell/shell_io.rs
@@ -2,24 +2,28 @@ use std::io::{BufReader, stdin, stdout};
 use std::io::prelude::*;
 use std::fs::File;
 use std::rc::Rc;
-
+use std::io::{Error, ErrorKind};
 
 #[derive(Debug)]
-struct Action {
-    action_name: String,
-    action_args: Vec<String>,
-    action_len: usize
+pub struct Action {
+    pub action_name: String,
+    pub action_args: Vec<String>,
+    pub action_len: usize
 }
 
 /*
     read_file - Read the content of a existing file
 */
-pub fn read_file(path: &str) -> Result<usize, std::io::Error> {
-    let file = File::open(path)?;
+pub fn read_file(action: &Action) -> Result<usize, Error> {
+    
+    if action.action_len == 0 {
+        return Err(std::io::Error::new(ErrorKind::Other, "No file path specified"))
+    }
+
+    let file = File::open(action.action_args.first().unwrap())?;
     let buff = BufReader::new(&file);
     let mut bytes_number: usize = 0;
 
-    println!("File path: {}\n- - - - - - - - - - - - -", path);
 
     for line in buff.lines() {
         let b = line.unwrap() + &"".to_string();
@@ -32,47 +36,55 @@ pub fn read_file(path: &str) -> Result<usize, std::io::Error> {
 
 
 pub fn read_stdin() -> Result<u64, std::io::Error> {
+    
+    println!("Zirox Lexer 0.0.1");
     'EventLoop: loop {
 
-        println!("Zirox Lexer 0.0.1");
 
-        print!("Configuration Loaded successfully> ");
+        let mut buffer = String::new();
+
+
+        print!("Lexer> ");
         stdout().flush().unwrap();
-        for line in stdin().lines() {
 
-            let line = line.unwrap();
-            let ref1 = Rc::new(&line);
-            let ref2 = Rc::new(&line);
+        stdin()
+            .read_line(&mut buffer)
+            .expect("Error reading from stdin");
+        
+        let buffer = buffer.trim().to_string(); 
+        let ref1 = Rc::new(&buffer);
+        let ref2 = Rc::new(&buffer);
 
-            let cmd_parsed: Vec<String> = ref1.split(" ")
-                            .map(|val| val.to_string())
-                            .collect();
+        let cmd_parsed: Vec<String> = ref1.split(" ")
+                        .map(|val| val.to_string())
+                        .collect();
 
-            if cmd_parsed.is_empty() || ref2.as_str().to_string().eq(""){
+
+
+        if cmd_parsed.is_empty() || ref2.as_str().to_string().eq(""){
                 continue;
-            }
+        }
 
-            let action = Action {
+        let action = Action {
                 action_name: cmd_parsed.first().unwrap().to_string(),
                 action_args: Vec::from(&cmd_parsed[1..]),
-                action_len: cmd_parsed.len()
-            };
-
-            println!("{:?}", action);
+                action_len: cmd_parsed.len() - 1
+        };
 
 
-            match action.action_name.as_str() {
+
+        match action.action_name.as_str() {
 
                 "Read" => {
-                    let b= read_file(cmd_parsed[1].as_str())?;
-                    println!("- - - - - - - - - - - - -\nBytes:{}", b);
+                    match read_file(&action) {
+                        Ok(bytes) => println!("{}", bytes),
+                        Err(e) => println!("{}", e)
+                    }
                 },
                 "Exit" => break 'EventLoop,
                 _ => println!("Command or binary not found"),
             }
-            print!("Lexer> ");
-            stdout().flush().unwrap();
-        }
+
     };
 
     Ok(1)
